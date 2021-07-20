58c533f93180e109274a3e266ee270db8329cba9
Mayankesh Mishra <mayankeshmishra100@gmail.com>
2021-07-20 21:30:22 +0530

    formik used for validation

--- src/pages/Login.page.tsx
+++ src/pages/Login.page.tsx
@@ -5,44 +5,34 @@ import { HiLockClosed, HiCheck } from 'react-icons/hi';
 import { FaSpinner, FaCheck } from 'react-icons/fa';
 import { FiUser, FiLock } from 'react-icons/fi';
 import {BiCheck} from 'react-icons/bi';
-
+import * as yup from 'yup';
+import {useFormik} from 'formik';
+import Input from '../components/Input';
 interface Props {
 
 }
 
 const LoginPage: React.FC<Props> = () => {
+    const myFormValidator = yup.object().shape({
+        email : yup.string().required().email(),
+        password : yup.string().required().min(8,({min})=>`Atleast ${min} charecters`)
+    });
+    const myForm= useFormik({
+        initialValues: {
+            email:"",
+            password:""
+        },
+        validationSchema:myFormValidator,
+        onSubmit:(data, helpers)=>{
+            console.log(data);
+            setTimeout(()=>{
+                helpers.setSubmitting(false);
+                history.push('/dashboard');
+            },5000)
+        }
+    });
 
-    const [data, setData] = useState({ email: 'mayankeshmishra100@gmail.com', password: '' });
-    const [touchedState, setTouchedState] = useState({ email: false, password: false });
-    const [submitting, setSubmitting] = useState(false);
-    const history = useHistory();
-
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setData({ ...data, [event.target.name]: event.target.value });
-    }
-    const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
-        setTouchedState({ ...touchedState, [event.target.name]: true });
-
-    }
-
-    let emailError = '';
-
-    if (!data.email) {
-        emailError = 'E-mail is required';
-    }
-    else if (!data.email.endsWith('@gmail.com')) {
-        emailError = 'Please Enter a valid email address';
-    }
-
-    let passwordError = '';
-    if (!data.password) {
-        passwordError = 'Please Enter a password';
-    }
-    else if (data.password.length < 8) {
-        passwordError = 'Password should be atleast 8 charecters';
-    }
-
-    const [showPassword, setShowPassword] = useState(false);
+    const history = useHistory();const [showPassword, setShowPassword] = useState(false);
     const [remember, setRemember]=useState(false);
 
     return (
@@ -61,34 +51,10 @@ const LoginPage: React.FC<Props> = () => {
                     </p>
                 </div>
 
-                <form className="" onSubmit={(event) => {
-                    event.preventDefault();
-                    if (emailError || passwordError) {
-                        console.log(emailError, passwordError);
-                        return;
-                    }
-                    setSubmitting(true);
-                    console.log(data);
-                    setTimeout(
-                        () => {
-                            setSubmitting(false);
-                            history.push('/dashboard');
-                        }, 5000)
-
-                }}>
+                <form className="" onSubmit={myForm.handleSubmit}>
                     <div className="pb-2">
-                        <div className='h-12 box-content relative flex border-b border-gray-200 pt-2.5 mb-6'>
-                            <label htmlFor="email-address" className="sr-only">Email address</label>
-                            <FiUser className="w-6 h-6 top-4 absolute fill-primary text-primary-300" />
-                            <input id="email-address" name="email" type="email" autoComplete="email" value={data.email} onBlur={(event) => handleBlur(event)} onChange={(event) => handleChange(event)} required className="appearance-none pl-9 pb-2.5 w-full outline-none" placeholder="Email address" />
-                        </div>
-                        {touchedState.email && <div className='text-xs text-red-600'>{emailError}</div>}
-                        <div className='h-12 box-content relative flex border-b border-gray-200 pt-2.5 mb-6'>
-                            <label htmlFor="password" className="sr-only">Password</label>
-                            <FiLock className="w-6 h-6 top-4 absolute fill-primary text-primary-300" />
-                            <input id="password" name="password" type="password" autoComplete="current-password" value={data.password} onBlur={(event) => handleBlur(event)} onChange={(event) => handleChange(event)} required className="appearance-none pl-9 pb-2.5 w-full outline-none" placeholder="Password" />
-                        </div>
-                        {touchedState.password && <div className='text-xs text-red-600'>{passwordError}</div>}
+                        <Input touched={myForm.touched.email} error={myForm.errors.email} {...myForm.getFieldProps('email')} id="email-address" type="email" autoComplete="current-email" required />
+                        <Input touched={myForm.touched.password} error={myForm.errors.password} {...myForm.getFieldProps('password')} id="password" type="password" autoComplete="current-password" required />
                     </div>
 
                     <div className="flex items-center justify-between">
@@ -140,10 +106,9 @@ const LoginPage: React.FC<Props> = () => {
                         <Link to='/'  className='text-primary-300'> Terms</Link>.
                         </p>
                     </div>
-                    {/* <div className='mt-5'>
+                    <div className='mt-5'>
                         <button
-                            disabled={(!touchedState.email || !touchedState.password || emailError !== '' || passwordError !== '') ? true : false
-                            }
+                            disabled={!myForm.isValid}
                             type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                             <span className="absolute left-0 inset-y-0 flex items-center pl-3">
 
@@ -151,8 +116,8 @@ const LoginPage: React.FC<Props> = () => {
                             </span>
                             Sign in
                         </button>
-                        {submitting && <FaSpinner className='animate-spin mt-5' />}
-                    </div> */}
+                        {myForm.isSubmitting && <FaSpinner className='animate-spin mt-5' />}
+                    </div>
                 </form>
             </div>
         </div >
